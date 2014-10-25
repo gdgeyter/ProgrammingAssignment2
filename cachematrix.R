ae45f0a4d1b41ed8c57937219bc11ea0f0de5b3f
Gert De Geyter <gertdegeyter@gmail.com>
2014-10-25 18:16:29 +0200

    This should be working:wq

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##This function creates a matrix which stores the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv  <- NULL
+    set  <- function(y){
+        x <<- y
+        inv <<- NULL 
+    }
+    get  <- function() x
+    setinverse  <- function(inverse) inv  <<- inverse
+    getinverse  <- function() inv
+    list(set= set, get = get, 
+         setinverse = setinverse, 
+         getinverse = getinverse)
+    
 }
 
+## This function either computes the inverse matriv or returns the cached
+##inverse matrix if present. 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv  <- x$getinverse()
+    if (!is.null(inv)){ #Checks if the inverse matrix was cached or not
+        message("getting cached data")
+        return(inv)
+    }
+    data  <- x$get()
+    inv  <- solve(data, ...) #calculates the inverse matrix
+    x$setinverse(inv)
+    inv
 }
